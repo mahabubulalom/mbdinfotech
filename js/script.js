22c9464370f2ba2f590fd64c274c61e510948ef6
mahabubulalom <mdmahabubulalomhira10@gmail.com>
2024-03-09 23:42:14 -0800

    first update

---
+++ js/script.js
@@ -0,0 +1,64 @@
+let counter = document.querySelectorAll(".counter")
+let arr = Array.from(counter)
+
+arr.map((item)=>{
+    let count = 0
+
+    function CounterUp(){
+        count++
+        item.innerHTML = count
+        if(count == item.dataset.number){
+            clearInterval(stop);
+        }
+    }
+    let stop = setInterval(
+        function(){
+            CounterUp();
+        },100/item.dataset.speed
+    );
+})
+
+
+
+
+
+$(function(){
+    $(".nav_item .nav_link").on('click', function(event) {
+        if (this.hash !== ""){
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
+});
+
+
+$('.testimonial-item').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed:2000,
+    arrows:true,
+    nextArrow: '.right',
+    prevArrow: '.left',
+
+  });
+
+$('.delivery__items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed:2000,
+    arrows:true,
+    nextArrow: '.right1',
+    prevArrow: '.left1',
+
+  });
---
+++ tailwin.config.js
@@ -0,0 +1,81 @@
+tailwind.config = {
+    theme: {
+      extend: {
+            colors: {
+                
+                
+                
+            },
+
+            boxShadow: {
+                'boxshadow':"0 5px 10px rgba(0, 0, 0, 0.2)",
+                'boxshadow2':"0 10px 20px  rgba(0, 0, 0, 0.2)",
+                'cardshadow':"16px rgba(17, 17, 26, 1)",
+            },
+
+            
+
+            fontFamily: {
+                quicksand:["'Quicksand', sans-serif;"],
+                sans:["'Work Sans', sans-serif;"],
+            },
+            
+            maxWidth: {
+                container: "1320px",
+            },
+
+            width: {
+                'w850': '850px',
+                'w615': '615px',
+
+                
+            },
+
+            borderRadius: {
+                'r50': '50px',
+                
+            },
+
+            spacing: {
+                '30': '30px',
+                '50': '50px',
+                '70': '70px',
+                '100': '100px',
+                '200': '200px',
+                '150': '150px',
+                '16.5': '16.5px',
+                '32.5': '32.5px',
+                
+            },
+
+            backgroundImage: {
+                banner1: "url('images/banner3.jpg')",
+                
+            },
+
+            transitionProperty: {
+                '3s': 'all linear 0.5s',
+            },
+
+            fontSize: {
+                a80: '80px',
+                a15: '15px',
+                a16: '16px',
+                a20: '20px',
+                a23: '23px',
+                a40: '40px',
+                a13: '13px',
+                a30: '30px',
+                
+            },
+
+            lineHeight: {
+                'extra-loose': '2.5',
+                'a46': '46px',
+                'a90': '90px',
+                'a37': '37px',
+            },
+            
+        }
+    }
+}
